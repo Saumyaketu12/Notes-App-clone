4d32b588da37de154dd8799de6305112753937ad
Saumyaketu123 <saumyaketu1908@gmail.com>
2025-08-24 16:32:03 +0530

    added cloudinary to store the images

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -11,3 +11,8 @@ export const PORT = process.env.PORT || 5000;
 export const MONGO_URI = process.env.MONGO_URI;
 export const JWT_SECRET = process.env.JWT_SECRET;
 export const APP_ORIGIN = process.env.APP_ORIGIN || 'http://localhost:5173';
+
+
+export const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME;
+export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY;
+export const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET;
