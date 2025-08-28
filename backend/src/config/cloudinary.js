de6976ba705c84d57345693521547e32bebba063
Saumyaketu123 <saumyaketu1908@gmail.com>
2025-08-28 17:59:53 +0530

    bug fixing 2

--- backend/src/config/cloudinary.js
+++ backend/src/config/cloudinary.js
@@ -6,10 +6,16 @@ import { CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET } from
 console.log('CLOUDINARY_CONFIG:', { CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET });
 
 
-cloudinary.config({
-  cloud_name: CLOUDINARY_CLOUD_NAME,
-  api_key: CLOUDINARY_API_KEY,
-  api_secret: CLOUDINARY_API_SECRET
-});
+if (CLOUDINARY_CLOUD_NAME && CLOUDINARY_API_KEY && CLOUDINARY_API_SECRET) {
+  cloudinary.config({
+    cloud_name: CLOUDINARY_CLOUD_NAME,
+    api_key: CLOUDINARY_API_KEY,
+    api_secret: CLOUDINARY_API_SECRET
+  });
+} else if (process.env.CLOUDINARY_URL) {
+  cloudinary.config(process.env.CLOUDINARY_URL);
+} else {
+  console.error("Cloudinary credentials are not set. Please check your environment variables.");
+}
 
 export default cloudinary;
