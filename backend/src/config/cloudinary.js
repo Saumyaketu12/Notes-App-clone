37bdddc549648392f28361931f04f39a8809aabb
Saumyaketu123 <saumyaketu1908@gmail.com>
2025-08-28 17:54:29 +0530

    bug fixing 1

--- backend/src/config/cloudinary.js
+++ backend/src/config/cloudinary.js
@@ -3,6 +3,9 @@
 import { v2 as cloudinary } from 'cloudinary';
 import { CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET } from './env.js';
 
+console.log('CLOUDINARY_CONFIG:', { CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET });
+
+
 cloudinary.config({
   cloud_name: CLOUDINARY_CLOUD_NAME,
   api_key: CLOUDINARY_API_KEY,
